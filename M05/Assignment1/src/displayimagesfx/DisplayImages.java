07b5f57b8d639ebd30f4a4a6dbdb4528ac6d6ba1
scooke15 <scooke15@ivytech.edu>
2024-10-10 05:57:40 -0400

    assignment1

--- M05/Assignment1/src/displayimagesfx/DisplayImages.java
+++ M05/Assignment1/src/displayimagesfx/DisplayImages.java
@@ -10,10 +10,8 @@ public class DisplayImages extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Create a GridPane layout
         GridPane pane = new GridPane();
 
-        // Load the images (replace the image paths with your actual paths)
         Image flag1 = new Image("/Images/flag1.gif");
         Image flag2 = new Image("/Images/flag2.gif");
         Image flag6 = new Image("/Images/flag6.gif");
@@ -25,14 +23,13 @@ public class DisplayImages extends Application {
         ImageView imageView3 = new ImageView(flag6);
         ImageView imageView4 = new ImageView(flag7);
 
-        // Add the ImageViews to the GridPane (row, column)
+        // add the ImageViews to the GridPane (row, column)
         pane.add(imageView1, 0, 0);
         pane.add(imageView2, 1, 0);
         pane.add(imageView3, 0, 1);
         pane.add(imageView4, 1, 1);
-        // Set the scene and the stage
-        Scene scene = new Scene(pane);
-        
+
+        Scene scene = new Scene(pane);      
         primaryStage.setScene(scene);
         
         primaryStage.show();
